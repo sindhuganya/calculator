4491ff8e03c03edd08669ac6193387f2bf68ea04
sindhuganya <sindhuganya@gmail.com>
2024-08-07 17:26:39 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,19 @@
+// Function to append values to the display
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+// Function to clear the display
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+// Function to evaluate the expression and display result
+function calculate() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
